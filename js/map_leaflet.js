fe9ea4f989d2b2b4e5404f6a8b47230bcd6c1a67
mfcamargoh <al383422@uji.es>
2019-03-03 10:25:26 +0100

    Personal Website

---
+++ js/map_leaflet.js
@@ -0,0 +1,19 @@
+﻿var mymap;
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+	mymap = L.map('map').setView([39.983538, -0.039943], 14);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+    }).addTo(mymap);
+    var marker = L.marker([39.983538, -0.039943]).addTo(mymap);
+};
+
+
+
+
